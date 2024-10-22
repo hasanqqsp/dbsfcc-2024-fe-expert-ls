ea19f76eeb7f9019e109ca3342638609d1a0ffff
Hasan Ismail Abdulmalik <hasanqqsp@gmail.com>
2024-10-22 13:50:58 +0700

    starter-session-4

---
+++ src/scripts/utils.js
@@ -0,0 +1,41 @@
+export function generateMusicItemUsingTemplate({
+  id,
+  artLink,
+  title,
+  signature,
+  audioLink,
+}) {
+  const musicItemTemplate = document.getElementById("musicitemtemplate");
+
+  const element = musicItemTemplate.content.cloneNode(true);
+
+  const article = element.querySelector("[data-musicid]");
+  article.setAttribute("data-musicid", id);
+
+  const musicImage = element.getElementById("musicImage");
+  musicImage.src = artLink;
+  musicImage.alt = title;
+
+  const musicTitle = element.getElementById("musicTitle");
+  musicTitle.textContent = title;
+
+  const musicSignature = element.getElementById("musicSignature");
+  musicSignature.innerHTML = signature;
+
+  const musicAudio = element.getElementById("musicAudio");
+  musicAudio.src = audioLink;
+  // TODO 5 : Tambahkan title pada musicAudio
+  musicAudio.setAttribute("title", title);
+
+  return element;
+}
+
+export function showLoading() {
+  const musicsLoader = document.getElementById("loader");
+  musicsLoader.style.display = "block";
+}
+
+export function hideLoading() {
+  const musicsLoader = document.getElementById("loader");
+  musicsLoader.style.display = "none";
+}
