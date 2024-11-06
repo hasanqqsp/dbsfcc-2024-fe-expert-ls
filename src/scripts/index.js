808911237b2e0cc75464256d95bbc9d9ec57a80b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-11-06 14:23:55 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,30 @@
-import "../styles/styles.css";
+import '../styles/styles.css'
 
-import App from "./views/app";
-const showButton = document.querySelector(".navbar-toggler");
-const sidebar = document.querySelector(".collapsible");
-const closeButton = document.querySelector(".close-button");
-const overlay = document.querySelector(".overlay");
-const content = document.querySelector("main");
+import App from './views/app'
+import swRegister from './utils/sw-register'
 
-const app = new App({ showButton, closeButton, sidebar, overlay, content });
+const showButton = document.querySelector('.navbar-toggler')
+const sidebar = document.querySelector('.collapsible')
+const closeButton = document.querySelector('.close-button')
+const overlay = document.querySelector('.overlay')
+const content = document.querySelector('main')
 
-window.addEventListener("hashchange", () => {
-    app.renderPage();
-});
-window.addEventListener("load", function () {
-    app.renderPage();
-});
+const app = new App({ showButton, closeButton, sidebar, overlay, content })
 
-document.addEventListener("DOMContentLoaded", () => {
-    const skipLinkElem = document.querySelector(".skip-to-content");
-    skipLinkElem.addEventListener("click", (event) => {
-        event.preventDefault();
-        document.querySelector("#main").focus();
-        document.querySelector("#main").scrollIntoView();
-    });
-});
+window.addEventListener('hashchange', () => {
+  app.renderPage()
+})
+window.addEventListener('load', function () {
+  app.renderPage()
+  swRegister()
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const skipLinkElem = document.querySelector('.skip-to-content')
+
+  skipLinkElem.addEventListener('click', (event) => {
+    event.preventDefault()
+    document.querySelector('#main').focus()
+    document.querySelector('#main').scrollIntoView()
+  })
+})
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
- 
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -27,6 +28,19 @@ module.exports = {
     ],
   },
   plugins: [
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) =>
+            url.href.startsWith('https://github-leaderboard-api.vercel.app/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'github-leaderboard-api',
+          },
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
@@ -40,4 +54,4 @@ module.exports = {
       ],
     }),
   ],
-};
+}
